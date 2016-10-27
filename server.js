050b51b5bcc2ae5de3cedafa199a9709711cefa6
PJC-1 <jumpzine@gmail.com>
2016-10-27 15:27:24 -0700

    the rest of the files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+var controllers = require("./controllers");
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/view/index.html');
+});
+
+app.get('/api', controllers.api.index);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
